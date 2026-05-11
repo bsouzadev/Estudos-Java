522aa7d36429fb4e09566719a1b7f2d05c18692c
bsouzadev <sjbruno41@gmail.com>
2026-05-11 16:03:20 -0300

    Parâmetros polimórficos (Polimorfismo) 3/5

--- src/com/brunodev/estudos/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/com/brunodev/estudos/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,22 +1,16 @@
 package com.brunodev.estudos.Npolimorfismo.servico;
 
 import com.brunodev.estudos.Npolimorfismo.dominio.Computador;
+import com.brunodev.estudos.Npolimorfismo.dominio.Produto;
 import com.brunodev.estudos.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calculaImpostoPC(Computador computador){
-        System.out.println("Relatorio de imposto do computador: ");
-        double taxaProduto = computador.taxaProduto();
-        System.out.println("Computador " + computador.getNome());
-        System.out.println("Valor " + computador.getValor());
-        System.out.println("Imposto a ser pago " + taxaProduto);
-    }
 
-    public static void calculaImpostoTomate(Tomate tomate){
-        System.out.println("Relatorio de imposto do tomate: ");
-        double taxaProduto = tomate.taxaProduto();
-        System.out.println("Tomate " + tomate.getNome());
-        System.out.println("Valor " + tomate.getValor());
-        System.out.println("Imposto a ser pago " + taxaProduto);
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatorio de imposto: ");
+        double imposto = produto.taxaProduto(); //metodo da interface que for "herdado"
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Valor: " + produto.getValor());
+        System.out.println("Imposto a ser pago: " + imposto);
     }
 }
--- src/com/brunodev/estudos/Npolimorfismo/test/ProdutoTest.java
+++ src/com/brunodev/estudos/Npolimorfismo/test/ProdutoTest.java
@@ -1,16 +1,18 @@
 package com.brunodev.estudos.Npolimorfismo.test;
 
 import com.brunodev.estudos.Npolimorfismo.dominio.Computador;
+import com.brunodev.estudos.Npolimorfismo.dominio.Produto;
 import com.brunodev.estudos.Npolimorfismo.dominio.Tomate;
 import com.brunodev.estudos.Npolimorfismo.servico.CalculadoraImposto;
 
-public class Produto {
-    static void main(String[] args) {
-        Computador computador = new Computador("Ryzen 2", 60);
-        Tomate tomate = new Tomate("Tomate cereja", 10);
 
-        CalculadoraImposto.calculaImpostoPC(computador);
+public class ProdutoTest {
+    static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 2", 60);
+        Produto produto1 = new Tomate("Tomate cereja", 10);
+
+        CalculadoraImposto.calcularImposto(produto);
         System.out.println("-------");
-        CalculadoraImposto.calculaImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(produto1);
     }
 }
