0d04f32798cffdf1909cd466de1e6882f16bf279
bsouzadev <sjbruno41@gmail.com>
2026-05-11 15:13:23 -0300

    Polimorfismo 2/5

---
+++ src/com/brunodev/estudos/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,21 @@
+package com.brunodev.estudos.Npolimorfismo.test;
+
+import com.brunodev.estudos.Npolimorfismo.dominio.Computador;
+import com.brunodev.estudos.Npolimorfismo.dominio.Produto;
+
+public class ProdutoTest02 {
+    //polimorfismo na pratica:
+    static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 3", 300);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.taxaProduto());
+
+        System.out.println("------");
+
+        Produto produto2 = new Computador("Espanhol", 23.99);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.taxaProduto());
+    }
+}
