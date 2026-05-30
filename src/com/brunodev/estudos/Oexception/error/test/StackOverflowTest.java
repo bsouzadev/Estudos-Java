9ed008bb3fcb4a9daa71c64a3704ff9bcdd3a8c3
bsouzadev <sjbruno41@gmail.com>
2026-05-30 16:03:41 -0300

    Exceções 2

--- src/com/brunodev/estudos/Oexception/error/test/StackOverflowTest.java
+++ src/com/brunodev/estudos/Oexception/error/test/StackOverflowTest.java
@@ -1,6 +1,6 @@
 package com.brunodev.estudos.Oexception.error.test;
 
-//Erro é algo que acontece na jvm e que "não" é possível recuperar em tempo de execução.
+//Erro é algo que acontece na jvm e que não é possível recuperar em tempo de execução.
 public class StackOverflowTest {
     public static void main(String[] args){
         recursividade();
---
+++ src/com/brunodev/estudos/Oexception/runtime/test/RunTimeExceptionTest.java
@@ -0,0 +1,8 @@
+package com.brunodev.estudos.Oexception.runtime.test;
+
+public class RunTimeExceptionTest {
+    static void main(String[] args) {
+        int[] a = {1,2};
+        System.out.println(a[2]);
+    }
+}
