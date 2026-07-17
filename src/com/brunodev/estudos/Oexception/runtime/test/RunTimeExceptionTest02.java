44a599f6bc4827b64c61216070e6eb24867aa059
bsouzadev <sjbruno41@gmail.com>
2026-07-16 23:04:57 -0300

    Arrumando comentarios sobre as RunTimeException

--- src/com/brunodev/estudos/Oexception/runtime/test/RunTimeExceptionTest02.java
+++ src/com/brunodev/estudos/Oexception/runtime/test/RunTimeExceptionTest02.java
@@ -1,25 +1,21 @@
 package com.brunodev.estudos.Oexception.runtime.test;
 
 public class RunTimeExceptionTest02 {
+
     static void main() {
-        try{
-            divisao(1,0);
-        } catch (ArithmeticException e){
-            System.out.println("Não é possível dividir por zero.");
-        }
+        divisao(1,0);
+        System.out.println("Não é possível dividir por zero.");
     }
 
     private static int divisao(int a, int b) throws ArithmeticException{
-        //tratamento externo: (Lembrando que pode fazer essa exceção ser passada para cima através do comando throws no cabeçalho do metodo, fazendo assim ela ser tratada na main com o uso de try catch).
-        if(b == 0) throw new ArithmeticException("Não é possível dividir por zero."); //Throw lança uma exceção. Mas nesse caso nem é nescessario fazer isso. Se b = 0, o própio progama já lança a exceção, e ela é passada para cima. Deixando somente nesse metodo return a/b.
-        return a/b;
+        //Essa exceção pode ser passada para cima através do comando throws, assim ela vai ser tratada na main com o uso de try catch.
 
-//      tratamento local:
-//        try {
-//            return a/b;
-//        } catch (ArithmeticException e){
-//            System.out.println("Não é possível dividir por zero");
-//        }
-//      return 0;
+        if(b == 0) throw new ArithmeticException("Não é possível dividir por zero.");//Throw lança uma exceção.
+
+        return a/b;
     }
 }
+
+
+//Checked Exception → throws é obrigatório se você não tratar a exceção no próprio metodo.
+//Unchecked Exception → throws é opcional. Serve apenas para indicar que o metodo pode lançar aquela exceção. Mesmo sem throws, a exceção é propagada (jogada para cima) automaticamente desde que ela não seja tratada no próprio metodo.
