73e065f3d8fc3d2565002191dced917ae072be77
bsouzadev <sjbruno41@gmail.com>
2026-07-22 13:38:49 -0300

    Strings pt 03 - Desempenho

---
+++ src/com/brunodev/estudos/Qstring/test/StringPerformaceTest.java
@@ -0,0 +1,41 @@
+package com.brunodev.estudos.Qstring.test;
+
+public class StringPerformaceTest {
+    static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(100_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " +(fim-inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(100_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder " +(fim-inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(100_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuffer " +(fim-inicio) + "ms");
+    }
+
+    private static void concatString(int tam){
+        String texto = "";
+        for (int i =0; i<tam; i++){
+            texto += i;
+        }
+    }
+
+    private static void concatStringBuilder(int tam){
+        StringBuilder sb = new StringBuilder(tam);
+        for (int i =0; i<tam; i++){
+            sb.append(i);
+        }
+    }
+
+    private static void concatStringBuffer(int tam){
+        StringBuffer sb = new StringBuffer(tam);
+        for (int i =0; i<tam; i++){
+            sb.append(i);
+        }
+    }
+}
