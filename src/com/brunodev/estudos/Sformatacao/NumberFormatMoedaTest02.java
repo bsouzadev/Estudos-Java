4987a89a26a09680de6377039a81181021d3f213
bsouzadev <sjbruno41@gmail.com>
2026-07-23 15:04:03 -0300

    Internacionalização de moeda com Locale

--- src/com/brunodev/estudos/Sformatacao/NumberFormatMoedaTest02.java
+++ src/com/brunodev/estudos/Sformatacao/NumberFormatMoedaTest02.java
@@ -4,7 +4,7 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatNumerosTest {
+public class NumberFormatMoedaTest02 {
     static void main(String[] args) {
         Locale localePT = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
@@ -12,20 +12,19 @@ public class NumberFormatNumerosTest {
 
         NumberFormat[] nb = new NumberFormat[4];
 
-        nb[0] = NumberFormat.getInstance();
-        nb[1] = NumberFormat.getInstance(localePT);
-        nb[2] = NumberFormat.getInstance(localeJP);
-        nb[3] = NumberFormat.getInstance(localeIT);
+        nb[0] = NumberFormat.getCurrencyInstance();
+        nb[1] = NumberFormat.getCurrencyInstance(localePT);
+        nb[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nb[3] = NumberFormat.getCurrencyInstance(localeIT);
 
         double valor = 10_000.2313;
         for (NumberFormat numberFormat : nb) {
-            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(valor));
         }
 
-        String valorSting = "10000.2313";
+        String valorSting = "￥10,000";
         try {
-            System.out.println(nb[2].parse(valorSting)); //Transforma o valor da String em um number.
+            System.out.println(nb[2].parse(valorSting)); //Transforma o valor da String numa moeda especifica.
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
