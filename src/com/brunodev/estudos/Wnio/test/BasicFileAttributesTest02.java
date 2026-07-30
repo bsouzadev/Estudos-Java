7fd51f1ff678e63f58bd74a02274cd69c1cf3ef9
bsouzadev <sjbruno41@gmail.com>
2026-07-30 16:25:35 -0300

    NIO pt 07 - BasicFileAttributes pt 02

--- src/com/brunodev/estudos/Wnio/test/BasicFileAttributesTest02.java
+++ src/com/brunodev/estudos/Wnio/test/BasicFileAttributesTest02.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 
 public class BasicFileAttributesTest02 {
     static void main(String[] args) throws IOException {
         Path path = Paths.get("pasta2/new.txt");
-        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class); //Criando um objeto do tipo basicFileAttributes. readAttributes retorna o que eu passar como objeto (que no caso foi BasicFileAttributes.class).
+
         FileTime creationTime = basicFileAttributes.creationTime();
         FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
         FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
@@ -18,5 +20,27 @@ public class BasicFileAttributesTest02 {
         System.out.println("creationTime " + creationTime);
         System.out.println("lastModifiedTime " + lastModifiedTime);
         System.out.println("lastAccessTime " + lastAccessTime);
+        System.out.println("----------");
+
+        //Alterando um dado utilizando as classes do pacote attribute. As classes que dão essa possibilidade de alterar alguns atributos terminam com view (BasicFileAttributesView, DosFileAttributesView, PosixFileAttributesView):
+
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime fileTime = FileTime.fromMillis(System.currentTimeMillis());
+        fileAttributeView.setTimes(lastModifiedTime,fileTime,creationTime); //Modificando a última data de acesso para agora
+
+        creationTime = fileAttributeView.readAttributes().creationTime();
+        lastModifiedTime = fileAttributeView.readAttributes().lastModifiedTime();
+        lastAccessTime = fileAttributeView.readAttributes().lastAccessTime();
+
+        System.out.println("creationTime " + creationTime);
+        System.out.println("lastModifiedTime " + lastModifiedTime);
+        System.out.println("lastAccessTime " + lastAccessTime);
+
     }
 }
+
+// BasicFileAttributes, DosFileAttributes, PosixFileAttributes servem para ler dados ocultos (metadados) de arquivos no Java.
+
+// BasicFileAttributes: Funciona em qualquer sistema. Mostra o básico: tamanho, data de criação, modificação e se é uma pasta ou arquivo.
+// DosFileAttributes: Feito para Windows. Mostra se o arquivo é oculto, somente leitura, de sistema ou arquivo morto.
+// PosixFileAttributes: Feito para Linux e macOS. Mostra o dono do arquivo, o grupo e as permissões de leitura/escrita (rwxrwxrwx).
