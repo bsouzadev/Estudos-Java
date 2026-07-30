b2a630ea462f0576085b83ad57f9941902294da3
bsouzadev <sjbruno41@gmail.com>
2026-07-30 20:30:07 -0300

    NIO pt 09 - PosixFileAttributes

---
+++ src/com/brunodev/estudos/Wnio/test/PosixFileAttributesTest01.java
@@ -0,0 +1,27 @@
+package com.brunodev.estudos.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+public class PosixFileAttributesTest01 {
+    static void main(String[] args) throws IOException {
+        //Lembrando que PosixFileAttributes é para o ambiente linux.
+
+        Path path = Paths.get("/home/bruno/dev/file.txt");
+        PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+        System.out.println(posixFileAttributes.permissions());
+
+        //Alterando as permissões do arq:
+        PosixFileAttributeView fileAttributeView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
+        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("rw-rw-rw-");
+        fileAttributeView.setPermissions(posixFilePermissions);
+        System.out.println(fileAttributeView.readAttributes().permissions());
+    }
+}
