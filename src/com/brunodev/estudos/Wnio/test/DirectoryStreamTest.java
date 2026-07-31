266206daaec3ef94365ffe04110c08da379e7abb
bsouzadev <sjbruno41@gmail.com>
2026-07-30 21:00:59 -0300

    NIO pt 10 - DirectoryStream

---
+++ src/com/brunodev/estudos/Wnio/test/DirectoryStreamTest.java
@@ -0,0 +1,24 @@
+package com.brunodev.estudos.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStreamTest {
+    static void main(String[] args) {
+        //Serve para listar os arquivos e pastas de um diretório de forma eficiente, usando pouca memória e permitindo o uso do laço for-each.
+        Path dir = Paths.get(".");
+
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for(Path path : stream){
+                System.out.println(path.getFileName());
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+//DirectoryStream<Path> vai ter um Path para cada arq que for encontrado dentro da pasta que estamos (estudos) passado o caminho inicial dentro de Files.newDirectoryStream(dir).
