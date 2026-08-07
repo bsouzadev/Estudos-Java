e1f7dc002fb896985337201f395aeca5c67ca50b
bsouzadev <sjbruno41@gmail.com>
2026-08-07 14:25:52 -0300

    Coleções pt 01 - equals pt 01 - comentarios

--- src/com/brunodev/estudos/Ycolecoes/test/EqualsTest.java
+++ src/com/brunodev/estudos/Ycolecoes/test/EqualsTest.java
@@ -4,8 +4,9 @@ import com.brunodev.estudos.Ycolecoes.dominio.Smartphone;
 
 public class EqualsTest {
     static void main(String[] args) {
-        //Equals das str compara se uma str é igual a outra (conteúdo).
-        //Equals da classe object compara se as variáveis de referência apontam para o mesmo objeto.
+        //Equals das str compara se uma str é igual a outra (conteúdo) pois o equals dela é sobre escrito.
+        //Equals da classe object compara se as variáveis de referência apontam para o mesmo objeto (tem como sobre escrever ele também).
+        //Lembrando que == para objetos compara a referência.
 
         Smartphone smartphone = new Smartphone("1ABC1", "iphone");
         Smartphone smartphone2 = new Smartphone("1ABC1", "iphone");
